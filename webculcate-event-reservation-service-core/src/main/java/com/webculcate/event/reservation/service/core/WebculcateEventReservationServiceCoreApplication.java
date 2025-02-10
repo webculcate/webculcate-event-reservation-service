e669e62230c84db34f1f8d79725ab57507e43035
webculcate <webculcate@gmail.com>
2025-02-10 19:56:30 +0530

    started the service

---
+++ webculcate-event-reservation-service-core/src/main/java/com/webculcate/event/reservation/service/core/WebculcateEventReservationServiceCoreApplication.java
@@ -0,0 +1,13 @@
+package com.webculcate.event.reservation.service.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebculcateEventReservationServiceCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebculcateEventReservationServiceCoreApplication.class, args);
+	}
+
+}
---
+++ webculcate-event-reservation-service-core/src/test/java/com/webculcate/event/reservation/service/core/WebculcateEventReservationServiceCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webculcate.event.reservation.service.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebculcateEventReservationServiceCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
